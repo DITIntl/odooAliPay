871373c5762a44ad1fe5a3852fab0aad1bfde2b9
粟雪峰 <feng@suxuefengdeMacBook-Pro.local>
2019-04-18 10:07:02 +0800

    修复加载文件顺序而导致安装报错的问题

--- alipay/__manifest__.py
+++ alipay/__manifest__.py
@@ -12,8 +12,8 @@
     'application': True,
     'auto_install': False,
     'data': [
-        'security/ir.model.access.csv',
         'security/alipay_groups.xml',
+        'security/ir.model.access.csv',
         'data/system_conf.xml',
         'views/menu.xml',
         'views/res_config_settings_views.xml',
